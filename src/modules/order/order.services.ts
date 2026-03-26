04217efff26770ae8048cbf1e1b08c4893cdb647
OscarArreolatics <oscararielarre@gmail.com>
2026-03-26 12:34:07 -0600

    Fix: order price type

--- src/modules/order/order.services.ts
+++ src/modules/order/order.services.ts
@@ -2,17 +2,35 @@ import { CreateOrderDTO, UpdateOrderDTO } from "./order.schema"
 import Order from "./order.model"
 import { ClientSession } from "mongoose"
 
+const mapOrder = (order: any) => ({
+  ...order.toObject(),
+  priceTotal: order.priceTotal?.toString(),
+});
+
+
 export class OrderService {
   static async createOrder( data: CreateOrderDTO, session?: ClientSession ) {
     return await Order.create([data], { session }).then(r => r[0])
   }
 
   static async getAll() {
-    return await Order.find();
+   const orders = await Order.find().lean();
+
+  return orders.map((order: any) => ({
+    ...order,
+    priceTotal: order.priceTotal?.toString(),
+  }));
   }
 
   static async getById(id: string) {
-    return await Order.findById(id);
+    const order = await Order.findById(id).lean();
+
+  if (!order) return null;
+
+  return {
+    ...order,
+    priceTotal: order.priceTotal?.toString(),
+  };
   }
 
   static async update(id: string, data: UpdateOrderDTO ) {
